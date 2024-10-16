e01ef122e081c4f680bc9598d5298d8ce3224af2
Ibrahim lisa <princelisa15@yahoo.com>
2024-10-16 10:17:35 -0700

    version 1

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+
+    // The main method - entry point for the Java program
+    public static void main(String[] args) {
+        // Print a simple message to the console
+        System.out.println("Hello, World!");
+
+        // Example of a basic variable declaration and usage
+        int number = 5;
+        System.out.println("The number is: " + number);
+
+        // Example of calling a method
+        greetUser("lisa");
+    }
+
+    // A simple method that prints a greeting for a given user
+    public static void greetUser(String name) {
+        System.out.println("Hello, " + name + "! Welcome to Java programming.");
+    }
+}
